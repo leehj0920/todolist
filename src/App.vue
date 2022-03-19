471302cf0fc09c0f103c53a07ebd0b9e2fff29c4
leehj0920 <null>
2022-03-19 09:51:03 +0000

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Vue입니다"/>
   </div>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
