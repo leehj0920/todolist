c3d5190c38619735846581ee04d93a89ae1912cd
leehj0920 <null>
2022-04-02 14:24:27 +0000

    todolist 뷰 적용

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -9,41 +9,15 @@
 					<li class="list_add_tit"><p>등록 하기</p></li>
 					<li><p><input type="text"><button>등 록</button></p></li>
 				</ul>
-				<!--// ul.list_add -->
+				<!--// ul.list_add -->  
 
 				<!-- ul.todo_list -->
 				<ul class="todo_list">
 					<li class="todo_list_tit"><p>할 일</p></li>
-
-
-
-					<li class="del_btn">
+					<li class="del_btn" v-for="item, index in todolist" :key="index">
 						<p>
-							<input type="checkbox" name="check1" value="">JS 공부하기
+							<input type="checkbox" name="check1" value="">{{ item.todo }}
 						</p>
-						<ul class="todo_list_btn">
-							<li><button>수 정</button></li>
-							<li><button>삭 제</button></li>
-						</ul>
-					</li>
-
-
-
-					<li>
-						<p><input type="checkbox" name="check2" value="">웹 알아보기</p>
-						<p><span></span></p>
-					</li>
-					<li>
-						<p><input type="checkbox" name="check3" value="">책 읽기</p>
-						<p><span></span></p>
-					</li>
-					<li>
-						<p><input type="checkbox" name="check4" value="">거북이한테 전화하기</p>
-						<p><span></span></p>
-					</li>
-					<li>
-						<p><input type="checkbox" name="check5" value="">유리랑 소꿉놀이하기</p>
-						<p><span></span></p>
 					</li>
 				</ul>
 				<!--// ul.todo_list -->
@@ -59,6 +33,13 @@ export default {
   name: 'todolist',
   data: function() {
 	  return {
+		  todolist:[
+			  {todo:"Vue.js공부하기"},
+			  {todo:"과제하기"},
+			  {todo:"엄마한테 전화하기"},
+			  {todo:"예원이 수학 테스트"},
+			  {todo:"운동하기"},
+		  ]
 	  }
   },
 }
